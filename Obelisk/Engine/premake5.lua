9cee2bead82c23f059435daa472ac485cdceadcc
Myles Cragg <cragg.myles@gmail.com>
2020-06-03 00:18:39 -0400

    Only move engine dll in release builds

--- Obelisk/Engine/premake5.lua
+++ Obelisk/Engine/premake5.lua
@@ -37,44 +37,46 @@ project "Obelisk-Engine"
         "_CRT_SECURE_NO_WARNINGS"
     }
 
-    postbuildcommands {
-        "{COPY} %{wks.location}build/bin/" .. outputdir .. "/Engine/ObeliskEngine.dll %{wks.location}build/bin/"..outputdir.."/Entry/",
-        "{COPY} %{wks.location}build/bin/" .. outputdir .. "/Engine/ObeliskEngine.dll %{wks.location}build/bin/"..outputdir.."/Editor/"
-    }
-
+    
     filter "system:windows"
-        systemversion "latest"
-
-        defines {
-            "WIN32_LEAN_AND_MEAN"
-        }
+    systemversion "latest"
+    
+    defines {
+        "WIN32_LEAN_AND_MEAN"
+    }
     
     filter "configurations:Debug"
-        defines { "OBL_DEBUG" }
-        runtime "debug"
-        optimize "off"
-        symbols "on"
+    defines { "OBL_DEBUG" }
+    runtime "debug"
+    optimize "off"
+    symbols "on"
     
     filter "configurations:Development"
-        defines { "OBL_DEVELOPMENT" }
-        runtime "release"
-        optimize "debug"
-        symbols "on"
+    defines { "OBL_DEVELOPMENT" }
+    runtime "release"
+    optimize "debug"
+    symbols "on"
     
     filter "configurations:ReleaseSymbols"
-        defines { "OBL_RELEASE" }
-        runtime "release"
-        optimize "speed"
-        symbols "on"
+    defines { "OBL_RELEASE" }
+    runtime "release"
+    optimize "speed"
+    symbols "on"
     
     filter "configurations:Release"
-        defines { "OBL_RELEASE" }
-        runtime "release"
-        optimize "speed"
-        symbols "off"
+    defines { "OBL_RELEASE" }
+    runtime "release"
+    optimize "speed"
+    symbols "off"
     
     filter "configurations:not Debug"
     
     filter "configurations:not Development"
     
     filter "configurations:not Release"
+    
+    filter { "configurations:not Debug", "configurations:not Development" }
+        postbuildcommands {
+            "{COPY} %{wks.location}build/bin/" .. outputdir .. "/Engine/ObeliskEngine.dll %{wks.location}build/bin/"..outputdir.."/Entry/",
+            "{COPY} %{wks.location}build/bin/" .. outputdir .. "/Engine/ObeliskEngine.dll %{wks.location}build/bin/"..outputdir.."/Editor/"
+        }
